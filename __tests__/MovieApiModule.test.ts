68bcd2ccc1efeb8bcba8dcf07b0bdea729a3f967
Ahmed Ezzat <ahmdezzat3@gmail.com>
2024-02-07 17:21:35 +0200

    add some mocks for api call unit test

--- __tests__/MovieApiModule.test.ts
+++ __tests__/MovieApiModule.test.ts
@@ -12,22 +12,44 @@ jest.mock('react-native', () => {
   };
 });
 
+const { MovieApiModule } = NativeModules;
+
+
 describe('MovieApiModule', () => {
   it('should fetch movies list', async () => {
-    const { fetchMoviesList } = NativeModules.MovieApiModule;
-    await fetchMoviesList('filter', 1);
-    expect(fetchMoviesList).toHaveBeenCalledWith('filter', 1);
+    // Set up test data
+    const filter = 'upcoming';
+    const page = 1;
+    const moviesList = [{ id: 1, title: 'Movie 1' }, { id: 2, title: 'Movie 2' }];
+    NativeModules.MovieApiModule.fetchMoviesList.mockResolvedValueOnce(moviesList);
+
+    const response = await MovieApiModule.fetchMoviesList({ filter, page });
+
+    expect(NativeModules.MovieApiModule.fetchMoviesList).toHaveBeenCalledWith({"filter": "upcoming", "page": 1});
+
+    expect(response).toEqual(moviesList);
   });
 
   it('should fetch genres', async () => {
-    const { fetchGenres } = NativeModules.MovieApiModule;
-    await fetchGenres();
-    expect(fetchGenres).toHaveBeenCalled();
+    const genresList = [{ id: 1, name: 'Genre 1' }, { id: 2, name: 'Genre 2' }];
+    NativeModules.MovieApiModule.fetchGenres.mockResolvedValueOnce(genresList);
+
+    const response = await MovieApiModule.fetchGenres();
+
+    expect(NativeModules.MovieApiModule.fetchGenres).toHaveBeenCalled();
+
+    expect(response).toEqual(genresList);
   });
 
   it('should fetch credits', async () => {
-    const { fetchCredits } = NativeModules.MovieApiModule;
-    await fetchCredits(123);
-    expect(fetchCredits).toHaveBeenCalledWith(123);
+    const movieId = 123;
+    const credits = { id: 123, cast: [{ name: 'Actor 1' }, { name: 'Actor 2' }] };
+    NativeModules.MovieApiModule.fetchCredits.mockResolvedValueOnce(credits);
+
+    const response = await MovieApiModule.fetchCredits(movieId);
+
+    expect(NativeModules.MovieApiModule.fetchCredits).toHaveBeenCalledWith(movieId);
+
+    expect(response).toEqual(credits);
   });
 });
